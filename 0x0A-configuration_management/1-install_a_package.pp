73345d81f9d17811e2f8ee1ffecca8e5adad00ad
hebo7865 <lihf95@gmail.com>
2024-03-23 03:25:15 +0200

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,11 @@
+#!/usr/bin/pup
 #install flask from pip3
 package { 'flask':
   ensure    => '2.1.0',
-  provider  => 'pip3',
+  provider  => 'pip',
 }
 
 package { 'werkzeug':
   ensure   => '2.1.1',
-  provider => 'pip3',
+  provider => 'pip',
 }
