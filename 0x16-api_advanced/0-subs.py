b98f26e7da3f255c2ace875d16bbcbdfe9a78065
hebo7865 <lihf95@gmail.com>
2024-05-10 02:46:28 +0300

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,11 +4,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """ returns the number of subscribers"""
+    """returns the number of subscribers"""
 
-    header = {'User-Agent': 'MyPythonScript/1.0'}
+    headers = {'User-Agent': 'MyPythonScript/1.0'}
     request = requests.get(
-        f"https://www.reddit.com/r/{subreddit}/about.json", header=header)
+        f"https://www.reddit.com/r/{subreddit}/about.json", headers=headers)
 
     if request.status_code == 200:
         subscribers = request.json().get('data').get('subscribers')
